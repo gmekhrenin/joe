d1ab53ba486cd668b559e5c0f0253e9f056e5170
akartasov <agneum@gmail.com>
2020-01-24 21:33:05 +0700

    refactor: switch commands

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -9,8 +9,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"gitlab.com/postgres-ai/joe/pkg/dblab"
-	"gitlab.com/postgres-ai/joe/pkg/provision"
 	"html"
 	"net/http"
 	"regexp"
@@ -30,7 +28,9 @@ import (
 	"gitlab.com/postgres-ai/database-lab/pkg/log"
 	"gitlab.com/postgres-ai/database-lab/pkg/models"
 	"gitlab.com/postgres-ai/joe/pkg/chatapi"
+	"gitlab.com/postgres-ai/joe/pkg/dblab"
 	"gitlab.com/postgres-ai/joe/pkg/pgexplain"
+	"gitlab.com/postgres-ai/joe/pkg/provision"
 	"gitlab.com/postgres-ai/joe/pkg/util"
 )
 
@@ -415,8 +415,6 @@ func (b *Bot) processAppMentionEvent(ev *slackevents.AppMentionEvent) {
 func (b *Bot) processMessageEvent(ev *slackevents.MessageEvent) {
 	var err error
 
-	explainConfig := b.Config.Explain
-
 	// Skip messages sent by bots.
 	if ev.User == "" || ev.BotID != "" {
 		log.Dbg("Message filtered: Bot")
@@ -650,190 +648,10 @@ func (b *Bot) processMessageEvent(ev *slackevents.MessageEvent) {
 
 	switch {
 	case command == COMMAND_EXPLAIN:
-		var detailsText string
-		var trnd bool
-
-		if query == "" {
-			failMsg(msg, MSG_EXPLAIN_OPTION_REQ)
-			b.failApiCmd(apiCmd, MSG_EXPLAIN_OPTION_REQ)
-			return
-		}
-
-		// Explain request and show.
-		var res, err = dbExplain(connStr, query)
-		if err != nil {
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		apiCmd.PlanText = res
-		planPreview, trnd := cutText(res, PLAN_SIZE, SEPARATOR_PLAN)
-
-		msgInitText := msg.Text
-
-		err = msg.Append(fmt.Sprintf("*Plan:*\n```%s```", planPreview))
-		if err != nil {
-			log.Err("Show plan: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		filePlanWoExec, err := b.Chat.UploadFile("plan-wo-execution-text", res, ch, msg.Timestamp)
-		if err != nil {
-			log.Err("File upload failed:", err)
-			failMsg(msg, err.Error())
-			return
-		}
-
-		detailsText = ""
-		if trnd {
-			detailsText = " " + CUT_TEXT
-		}
-
-		err = msg.Append(fmt.Sprintf("<%s|Full plan (w/o execution)>%s", filePlanWoExec.Permalink, detailsText))
-		if err != nil {
-			log.Err("File: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		// Explain analyze request and processing.
-		res, err = dbExplainAnalyze(connStr, query)
-		if err != nil {
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		apiCmd.PlanExecJson = res
-
-		// Visualization.
-		explain, err := pgexplain.NewExplain(res, explainConfig)
-		if err != nil {
-			log.Err("Explain parsing: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		vis := explain.RenderPlanText()
-		apiCmd.PlanExecText = vis
-
-		planExecPreview, trnd := cutText(vis, PLAN_SIZE, SEPARATOR_PLAN)
-
-		err = msg.Replace(msgInitText + chatapi.CHAT_APPEND_SEPARATOR +
-			fmt.Sprintf("*Plan with execution:*\n```%s```", planExecPreview))
-		if err != nil {
-			log.Err("Show the plan with execution:", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		_, err = b.Chat.UploadFile("plan-json", res, ch, msg.Timestamp)
-		if err != nil {
-			log.Err("File upload failed:", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		filePlan, err := b.Chat.UploadFile("plan-text", vis, ch, msg.Timestamp)
-		if err != nil {
-			log.Err("File upload failed:", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		detailsText = ""
-		if trnd {
-			detailsText = " " + CUT_TEXT
-		}
-
-		err = msg.Append(fmt.Sprintf("<%s|Full execution plan>%s \n"+
-			"_Other artifacts are provided in the thread_",
-			filePlan.Permalink, detailsText))
-		if err != nil {
-			log.Err("File: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		// Recommendations.
-		tips, err := explain.GetTips()
-		if err != nil {
-			log.Err("Recommendations: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		recommends := ""
-		if len(tips) == 0 {
-			recommends += ":white_check_mark: Looks good"
-		} else {
-			for _, tip := range tips {
-				recommends += fmt.Sprintf(
-					":exclamation: %s – %s <%s|Show details>\n", tip.Name,
-					tip.Description, tip.DetailsUrl)
-			}
-		}
-
-		apiCmd.Recommendations = recommends
-
-		err = msg.Append("*Recommendations:*\n" + recommends)
-		if err != nil {
-			log.Err("Show recommendations: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		// Summary.
-		stats := explain.RenderStats()
-		apiCmd.Stats = stats
-
-		err = msg.Append(fmt.Sprintf("*Summary:*\n```%s```", stats))
-		if err != nil {
-			log.Err("Show summary: ", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
+		Explain(b, query, apiCmd, msg, ch, connStr)
 
 	case command == COMMAND_EXEC:
-		if query == "" {
-			failMsg(msg, MSG_EXEC_OPTION_REQ)
-			b.failApiCmd(apiCmd, MSG_EXEC_OPTION_REQ)
-			return
-		}
-
-		start := time.Now()
-		err = dbExec(connStr, query)
-		elapsed := time.Since(start)
-		if err != nil {
-			log.Err("Exec:", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
-
-		duration := util.DurationToString(elapsed)
-		result := fmt.Sprintf("The query has been executed. Duration: %s", duration)
-		apiCmd.Response = result
-
-		err = msg.Append(result)
-		if err != nil {
-			log.Err("Exec:", err)
-			failMsg(msg, err.Error())
-			b.failApiCmd(apiCmd, err.Error())
-			return
-		}
+		Exec(b, query, apiCmd, msg, connStr)
 
 	case command == COMMAND_SNAPSHOT:
 		if query == "" {
@@ -1021,6 +839,14 @@ func failMsg(msg *chatapi.Message, text string) {
 }
 
 func (b *Bot) failApiCmd(apiCmd *ApiCommand, text string) {
+	apiCmd.Error = text
+	_, err := b.ApiPostCommand(apiCmd)
+	if err != nil {
+		log.Err("failApiCmd:", err)
+	}
+}
+
+func (b *Bot) FailApiCmd(apiCmd *ApiCommand, text string) {
 	apiCmd.Error = text
 	_, err := b.ApiPostCommand(apiCmd)
 	if err != nil {
